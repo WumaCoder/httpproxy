19245a9446e2474b249af7dcb453ce65c123d110
WumaCoder <1829913225@qq.com>
2023-12-26 10:31:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+async / await
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: false })
+
+// Declare a route
+fastify.post('/fetch', async (request, reply) => {
+  return await axios(request.body).then(res => {
+    return {
+      status: res.status,
+      statusText: res.statusText,
+      data: res.data,
+      headers: res.headers,
+    }
+  });
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000, '0.0.0.0')
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
